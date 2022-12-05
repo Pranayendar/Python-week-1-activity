62e5bdd38fae42fb2158c042e3b61372e5a1d808
Pranayendar <114793932+Pranayendar@users.noreply.github.com>
2022-12-05 22:51:43 +0530

    Create lecture 36-Striping Spaces-CipherSchools.py

---
+++ lecture 36-Striping Spaces-CipherSchools.py
@@ -0,0 +1,19 @@
+# Striping the spaves
+
+
+dot="...................."
+name="         Dheeraj          "
+dots="......................."
+# lstrip()   Method
+print(dot+name+dots)
+print(dot+name.lstrip()+dots)
+# rstrip()  Method
+print(dot+name.rstrip()+dots)
+# rstrip() and lstrip()  Method   or strip()
+print(dot+name.lstrip().rstrip()+dots)
+print(dot+name.strip()+dots)
+print(dot+name.replace(" ","")+dots)
+
+# ! To remove all spaces
+a="Ha   R h  jk   ajk  ks "
+print(a.replace(" ",""))
