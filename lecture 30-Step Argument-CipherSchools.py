26c2b7774b4856313bf676f3bba66095cba09d3b
Pranayendar <114793932+Pranayendar@users.noreply.github.com>
2022-12-05 22:45:24 +0530

    Create lecture 30-Step Argument-CipherSchools.py

---
+++ lecture 30-Step Argument-CipherSchools.py
@@ -0,0 +1,8 @@
+lan="Python"
+#^ Syntax - Variable[Start argument : Stop Argument - 1:Increment/Decrement]
+                                #^  : count the position       
+                                
+print("Dheerajasdasdasdas"[0:9:2])
+
+# !to reverse
+print("Dheeraj"[-1::-1])
