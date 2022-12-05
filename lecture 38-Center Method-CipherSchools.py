9745cce6cbf522eeef118ccf0f242a99a7d575a5
Pranayendar <114793932+Pranayendar@users.noreply.github.com>
2022-12-05 22:53:21 +0530

    Create lecture 38-Center Method-CipherSchools.py

---
+++ lecture 38-Center Method-CipherSchools.py
@@ -0,0 +1,12 @@
+name="pranay"
+#^ to print **pranay**  len=10
+#! Syntax:- variable.center(len of expected output,"to be printed extra") 
+print(name.center(10,"!"))
+print(name.center(9,"@"))
+print(name.center(7,"*"))
+
+
+nam=input("Enter your name: ")
+ch=input("enter the character you want to input: ")
+no=int(input("Enter the no of times the char should apply: "))
+print(nam.center((len(nam)+no), ch))
